957bc280d2f79259871b4515ded34cef02321a6c
Rohit-katkar2003 <katkarrohit23@gmail.com>
2024-12-28 11:37:11 +0530

    Created prompt logic

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+prompt_template = """
+use the following piece of information to answer the user questions. 
+sorry, i don't know the answer.
+
+Context : {context}
+Question: {question}
+
+only return the helpful answer below and nothing else.
+Helpful answer: """

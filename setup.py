3b142a8ed6787f708c10318cf4680780fb568f61
Rohit-katkar2003 <katkarrohit23@gmail.com>
2024-12-28 11:37:52 +0530

    Setup script added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup 
+
+
+setup(
+    name = "Generative AI project" , 
+    version= '0.0.0' , 
+    author= "rohit katkar"  , 
+    author_email= "katkarrohit203@gmail.com" , 
+    packages=find_packages() , 
+    install_requires = []
+)
